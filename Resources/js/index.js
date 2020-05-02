b82fca89141b7dbf3a8206ad947f12d7899de11b
acerbicinq <acerbicinq@pm.me>
2020-05-02 10:37:53 -0400

    Fixed bugs in media queries.

--- Resources/js/index.js
+++ Resources/js/index.js
@@ -4,20 +4,11 @@
 
 
 
-
-
-
-
-//Write code for animated scroll Buttons
-
-
-
-
 //Portfolio onHover Animations
 const quiz = document.getElementById('quiz');
 const quizContent = document.getElementById('quiz-content');
-const salon = document.getElementById('salon');
-const salonContent = document.getElementById('salon-content');
+const writer = document.getElementById('writer');
+const writerContent = document.getElementById('writer-content');
 const multisite = document.getElementById('multisite');
 const multisiteContent = document.getElementById('multisite-content');
 
@@ -31,14 +22,14 @@ quiz.addEventListener('mouseout', function(e) {
   quizContent.classList.remove('visible');
 });
 
-//Salon
-salon.addEventListener('mouseover', function(e) {
-  salonContent.classList.add('visible');
-  salonContent.classList.remove('hidden');
+//Writer's App
+writer.addEventListener('mouseover', function(e) {
+  writerContent.classList.add('visible');
+  writerContent.classList.remove('hidden');
 });
-salon.addEventListener('mouseout', function(e) {
-  salonContent.classList.add('hidden');
-  salonContent.classList.remove('visible');
+writer.addEventListener('mouseout', function(e) {
+  writerContent.classList.add('hidden');
+  writerContent.classList.remove('visible');
 });
 
 //Multisite
@@ -50,7 +41,3 @@ multisite.addEventListener('mouseout', function(e) {
   multisiteContent.classList.add('hidden');
   multisiteContent.classList.remove('visible');
 });
-
-
-
-//Write code for media queries swapping diff views on diff devices
